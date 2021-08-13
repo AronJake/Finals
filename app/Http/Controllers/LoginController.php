de0eeaa7f1eea5343980944d353a794e1349f710
AronJake <87650118+AronJake@users.noreply.github.com>
2021-08-13 23:17:01 +0800

    Add files via upload

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    public function login(Request $request){
+        $request->validate([
+            'email'=> ['required','email'],
+            'password'=> ['required']
+        ]);
+
+        if(Auth::attempt($request->only('email','password'))){
+            return response()->json(Auth::user(),200);
+        }
+        throw ValidationException::withMessages([
+            'email'=>['The provided credentials are incorrect.']
+        ]);
+    }
+
+    public function logout(){
+        Auth::logout();
+    }
+}
